a7f1d4cf3eac6c4ea7f2a96023dc6042beb5eaa4
Amanmishra03 <aman274501mishra@gmail.com>
2023-09-23 11:07:41 +0530

    udemy

--- day10.py/sum_even_no.py
+++ day10.py/sum_even_no.py
@@ -1,17 +1,17 @@
-# num = int(input("enter the number: "))
-# sum = 0
-# for i in range(1,num):
-#     if i % 2 ==0:
-#         print(i)
-#         sum = sum + i
-# print(sum)
+num = int(input("enter the number: "))
+sum = 0
+for i in range(1,num):
+    if i % 2 ==0:
+        # print(i)
+        sum = sum + i
+print(sum)
 
-no_of_subject = int(input("enter the no. of subject: "))
-marks_obtained =input("enter marks obtained")
-num = int(marks_obtained)
-# marks_obtained = (10, 20, 30, 40, 50)
+# no_of_subject = int(input("enter the no. of subject: "))
+# marks_obtained =input("enter marks obtained")
+# num = int(marks_obtained)
+# # marks_obtained = (10, 20, 30, 40, 50)
 
-total_marks = sum(num)
-avg = total_marks/no_of_subject
-print(avg)
+# total_marks = sum(num)
+# avg = total_marks/no_of_subject
+# print(avg)
 
