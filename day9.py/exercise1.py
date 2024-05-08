427973f4a913a03d213d6611b6758e87f4dccb0c
Amanmishra03 <aman274501mishra@gmail.com>
2024-05-08 18:45:13 +0530

    udemy

--- day9.py/exercise1.py
+++ day9.py/exercise1.py
@@ -9,17 +9,17 @@ student_grade = {}
 
 for student in student_score:
   print(student)
-  # score = student_score[student]
-  # print(score)
-#   if score > 90:
-#     student_grade[student] = "Outstanding"
-#   elif score > 80:
-#     student_grade[student] = "ExceedsExpections"
-#   elif score > 70:
-#     student_grade[student] = "Acceptable"
-#   else:
-#     student_grade[student] = "Fail"
+  score = student_score[student]
+  print(score)
+  if score > 90:
+    student_grade[student] = "Outstanding"
+  elif score > 80:
+    student_grade[student] = "ExceedsExpections"
+  elif score > 70:
+    student_grade[student] = "Acceptable"
+  else:
+    student_grade[student] = "Fail"
 
-# print(student_grade)
+print(student_grade)
     
 
