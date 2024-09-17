dc5217e784e4359179d24de5e9e1df0a72fd6fbe
Amanmishra03 <aman274501mishra@gmail.com>
2024-09-17 11:02:54 +0530

    udemy

--- day29.py/main.py
+++ day29.py/main.py
@@ -8,6 +8,7 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
+window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
