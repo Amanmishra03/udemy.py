51c7ed819a84aa63f82d34c89cfb3c47c0a3eed5
Amanmishra03 <aman274501mishra@gmail.com>
2025-04-01 16:19:47 +0530

    api

--- day33/api.py
+++ day33/api.py
@@ -1,9 +1,9 @@
 import requests
 
 response = requests.get('http://api.open-notify.org/iss-now.json')
-# print(response.status_code)
-# print(response)
-
-if response.status_code != 200:
-    # print('There was an error')
-    raise Exception('Bad response from ISS API')
+response.raise_for_status() 
+data = response.json()
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+iss_position = (longitude, latitude)
+print(iss_position)
