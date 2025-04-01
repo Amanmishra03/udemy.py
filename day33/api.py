c13aaf2c3e1dc07887b05c1d5e3d64ac9e7a9344
Amanmishra03 <aman274501mishra@gmail.com>
2025-04-01 15:52:56 +0530

    api

--- day33/api.py
+++ day33/api.py
@@ -1,4 +1,9 @@
 import requests
 
 response = requests.get('http://api.open-notify.org/iss-now.json')
-print(response)
+# print(response.status_code)
+# print(response)
+
+if response.status_code != 200:
+    # print('There was an error')
+    raise Exception('Bad response from ISS API')
